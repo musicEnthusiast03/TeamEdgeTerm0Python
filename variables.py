79f735bccb70b9e9f5be124f7f718c0e1ad3be85
Paul G <35400575+musicEnthusiast03@users.noreply.github.com>
2022-06-27 13:43:23 -0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -24,39 +24,23 @@ print(f"A fun fact about me is that {funFact}")
 # **** Challenge 1: Problem 1 ****
 # Initalize the variables with your response for the three statements above! 
 # Hint: Also think about where you would initialize those variables
-name = "Paul
-grade = 14
-funFact = "I am going to concerts for The Kid Laroi and Sasha Sloan this summer"
-print(f"Hi! My name is {name}")
-print(f"I'm in the {grade}")
-print(f"A fun fact about me is that {funFact}")
+
 
 # **** Upchallenge! ****
 # Can you also print out the data type of the variables? 
 
-type(name)
-type(grade)
-type(funFact)
 
 # **** Challenge 1: Problem 2 ****
 # Create a string for the different lines of the Happy Birthday Song
 # Print out the song using the strings you declared.
 
-line1 = "Happy Birthday to You"
-line2 = "Happy Birthday to You"
-line3 = "Happy Birthday Dear (name)"
-line4 = Happy Birthday to You.
-print(f"{line1}\n{line2}\n{line3}\n{line4}
+
 
 # **** Challenge 1: Problem 3 ****
 # Print out a string that has brackets in it.
 
 
-line1 = "Happy Birthday to You"
-line2 = "Happy Birthday to You"
-line3 = "Happy Birthday Dear (name)"
-line4 = Happy Birthday to You.
-print(f"{line1}\n{line2}\n{line3}\n{line4}
+
 
 
 # -------------------------------------------- 
@@ -75,41 +59,30 @@ num2 = 10
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
 
-nums = num1+num2
-print(nums)
 
 # **** Challenge 2: Problem 2 ****
 # Store the difference between sum (above) and 7 in a variable.
 # Print the difference.
 
-diff = sum-7
-print(diff)
+
 # **** Challenge 2: Problem 3 ****
 # Store the product of the difference and 3 in a variable.
 # Print the product.
 
-prod = diff*3
-print(prod)     
-      
+
 # **** Challenge 2: Problem 4 ****
 # Store the power of the product squared in a variable.
 # Print the power.
 
-power = prod^2
-print(power)   
-      
 # **** Challenge 2: Problem 5 ****
 # Store the quotient of the power divided by 4 in a variable.
 # Print the quotient.
 
-quot = power/4
-print(quot)   
-      
+
 # **** Challenge 2: Problem 6 ****
 # Store the remainder of the quotient divided by 2 in a variable.
 # Print the remainder.
-rem = quot%4
-print(rem)   
+
 
 # **** Upchallenge! ****
 # Given a variable that stores a number, print the values that come right before and right after.
@@ -117,7 +90,6 @@ print(rem)
 # Your code should work if the value of the variable is changed.
 
 num = 9
-print(num+1)
-print(num-1)   
+
 
 # -------------------------------------------- 
