44c426fa332aae6f48fd4d0e3eeccc021ad12dfb
Paul G <35400575+notpaulgaddy@users.noreply.github.com>
2022-07-11 10:43:40 -0400

    Update while_loops.py
    Challenge 2 done

--- while_loops.py
+++ while_loops.py
@@ -51,7 +51,9 @@ items = ["pencil" , "eraser" , "mirror" , "comb" , "spoon" , "key" , "earrings" 
 
 #-->TODO: Use a while loop to search the contents of a list for the key! If it exists, print "found the key!"
 
-
+while "key" in items:
+    print("found the key!")
+    break
 
 print("------------------- CHALLENGE 3 : BUGGIN   -------------------")
 
@@ -116,13 +118,16 @@ print("------------------- CHALLENGE 5 : WHAT AM I?   -------------------")
 keep_asking = False
 
 def prompt_user():
+    ask = input("Enter a word")
+    response(ask)
     pass
 
 def response(response):
+    print(f"I know you are a {ask} but what am I?")
     pass
 
 while keep_asking:
-    #response(prompt_user())
+    response(prompt_user())
     pass
 
 #-->TODO: Challenge! write a secret word to break out of the loop!
